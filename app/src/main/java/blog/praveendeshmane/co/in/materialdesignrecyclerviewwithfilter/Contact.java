cb34727caa305de43088f474421ffbd4373e0d8d
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-14 00:11:34 +0530

    Initial commit

---
+++ app/src/main/java/blog/praveendeshmane/co/in/materialdesignrecyclerviewwithfilter/Contact.java
@@ -0,0 +1,26 @@
+package blog.praveendeshmane.co.in.materialdesignrecyclerviewwithfilter;
+
+/**
+ * Created by Admin on 27-11-2017.
+ */
+
+public class Contact {
+    String name;
+    String image;
+    String phone;
+
+    public Contact() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
